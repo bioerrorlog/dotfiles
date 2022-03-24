c86cf3714b40335f547cd3b9a3f5e7b35d511e93
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-03-24 22:01:34 +0900

    add plug.vim

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,12 @@ cd $BASEDIR
 DOTFILES=(
     .vimrc
     .vim/colors/hybrid.vim
+    .vim/autoload/plug.vim
     .gitconfig
 )
 
 mkdir -p ~/.vim/colors
+mkdir -p ~/.vim/autoload
 
 for f in ${DOTFILES[@]}; do
     ln -snfv ${PWD}/$f ~/$f
